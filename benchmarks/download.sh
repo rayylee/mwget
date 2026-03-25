f394f92d85b72397d552407dcba4772df77542b9
Ray Lee <hburaylee@gmail.com>
2026-03-25 19:10:59 +0800

    benchmark: refactor benchmark using tmux
    Signed-off-by: Ray Lee <hburaylee@gmail.com>

---
+++ benchmarks/download.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+#
+
+cmd="mwget"
+args=""
+size="small"
+declare -A urls=(
+    ["small"]="http://127.0.0.1:8989/10MB.data"
+    ["mid"]="http://127.0.0.1:8989/50MB.data"
+    ["large"]="http://127.0.0.1:8989/200MB.data"
+)
+# declare -A urls=(
+#     ["small"]="http://speedtest.tele2.net/5MB.zip"
+#     ["mid"]="http://speedtest.tele2.net/50MB.zip"
+#     ["large"]="http://speedtest.tele2.net/100MB.zip"
+# )
+
+result="result.data"
+
+ARG1="$1"
+ARG2="$2"
+
+if [ -n "$ARG1" ]; then
+    case "$ARG1" in
+        mwget)
+            cmd=mwget
+            args="-O mwget.download"
+            ;;
+        wget)
+            cmd="wget"
+            args="-O wget.download"
+            ;;
+        curl)
+            cmd="curl"
+            args="-o curl.download"
+            ;;
+        *)
+            ;;
+    esac
+fi
+
+if [ -n "$ARG2" ]; then
+    case "$ARG2" in
+        small|mid|large)
+            size=$ARG2
+            ;;
+        *)
+            ;;
+    esac
+
+fi
+
+t1=$(date +%s%3N)
+$cmd $args "${urls[$size]}"
+t2=$(date +%s%3N)
+
+echo "$size $cmd $((t2-t1))" >> "$result"
+
+exit 0
